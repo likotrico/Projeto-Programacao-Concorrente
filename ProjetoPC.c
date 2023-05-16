ce345735157bf8b1abbd8cd3416f8c1398f220d4
likotrico <112947493+likotrico@users.noreply.github.com>
2023-05-16 09:53:33 -0300

    Versão serial corrigida

--- ProjetoPC.c
+++ ProjetoPC.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
-#define TAM 100
+#define TAM 1000
 #define TRUE 1
 #define FALSE 0
 
@@ -10,32 +11,53 @@ struct no{
     int marcado;
 };
 
+struct no *gerarVetor(int num){
+    int i;
+    struct no *conjunto;
+    conjunto = (struct no*)malloc(sizeof(struct no)*num);
+    for(i = 0; i < num; i++){
+        conjunto[i].num = i;
+        conjunto[i].marcado = TRUE;
+    }
+    conjunto[0].marcado = FALSE;
+    conjunto[1].marcado = FALSE;
+    return conjunto;
+}
+
+/*void resetarVetor(struct no* v){
+    int i;
+    for(i = 2; i < TAM; i++){
+        v[i].marcado = FALSE; 
+    }
+}*/
 
 int main(){
 
-    int i;
-    struct no conjunto[TAM];
-    for(i = 0; i < TAM; i++){
-        conjunto[i].num = i;
-        conjunto[i].marcado = FALSE;
-    }
-    conjunto[0].marcado = TRUE;
-    conjunto[1].marcado = TRUE;
-    
+    struct no* v;
+    v = gerarVetor(TAM);
+    int i, j;
 
     /*VERSÃO SEQUENCIAL*/
     
     for(i = 2; i < TAM; i++){
-        if(i != 2 && i != 3 && i != 5 && i != 7){
-            if(conjunto[i].num % 2 == 0 || conjunto[i].num % 3 == 0 || conjunto[i].num % 5 == 0 || conjunto[i].num % 7 == 0){
-                conjunto[i].marcado = TRUE;
+        if(v[i].marcado == TRUE){
+            for(j = v[i+1].num; j < TAM; j++){
+                if(v[j].num % v[i].num == 0) v[j].marcado = FALSE;
             }
         }
     }
+
     /*teste*/
+    int c = 0;
     for(i = 2; i < TAM; i++){
-        if(conjunto[i].marcado == 0) printf("%d\t", conjunto[i].num);
+        if(v[i].marcado == TRUE){
+            printf("%d\t", v[i].num);
+            c++;
+        }
     }
+
+    /*VERSÃO MPI*/
+    
     
     return 0;
 }
