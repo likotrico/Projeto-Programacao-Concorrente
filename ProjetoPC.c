53792be03f23fca8f6387ddc7d3cc8cefe4d89dd
likotrico <112947493+likotrico@users.noreply.github.com>
2023-05-17 13:59:34 -0300

    Add files via upload

--- ProjetoPC.c
+++ ProjetoPC.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <omp.h>
+#include <time.h>
 
-#define TAM 1000
+#define TAM 100
 #define TRUE 1
 #define FALSE 0
 
@@ -24,21 +26,23 @@ struct no *gerarVetor(int num){
     return conjunto;
 }
 
-/*void resetarVetor(struct no* v){
+void resetarVetor(struct no* v){
     int i;
     for(i = 2; i < TAM; i++){
-        v[i].marcado = FALSE; 
+        v[i].marcado = TRUE; 
     }
-}*/
+}
 
 int main(){
+    time_t t;
+    srand((unsigned) time(&t));
 
     struct no* v;
     v = gerarVetor(TAM);
     int i, j;
 
     /*VERSÃO SEQUENCIAL*/
-    
+    double inicio = omp_get_wtime();
     for(i = 2; i < TAM; i++){
         if(v[i].marcado == TRUE){
             for(j = v[i+1].num; j < TAM; j++){
@@ -46,15 +50,48 @@ int main(){
             }
         }
     }
+    double fim = omp_get_wtime();
+    double tSerial = fim - inicio;
+    printf("Execucao Serial: %fseg\n", tSerial);
 
     /*teste*/
-    int c = 0;
+    /*int c = 0;
     for(i = 2; i < TAM; i++){
         if(v[i].marcado == TRUE){
             printf("%d\t", v[i].num);
             c++;
         }
     }
+    printf("C: %d\n", c);*/
+
+    resetarVetor(v);
+    /*VERSÃO OPENMP*/
+    inicio = omp_get_wtime(); 
+    for(i = 2; i < TAM; i++){
+        if(v[i].marcado == TRUE){
+            #pragma omp parallel num_threads(4)
+            {
+                #pragma omp parallel for 
+                for(j = v[i+1].num; j < TAM; j++){
+                    if(v[j].num % v[i].num == 0) v[j].marcado = FALSE;
+                }
+            }
+            
+        }
+    }
+    fim = omp_get_wtime();
+    tSerial = fim - inicio;
+    printf("Execucao openmp: %fseg\n", tSerial);
+    printf("\n");
+
+    /*c = 0;
+    for(i = 2; i < TAM; i++){
+        if(v[i].marcado == TRUE){
+            printf("%d\t", v[i].num);
+            c++;
+        }
+    }
+    printf("count: %d\t", c);*/
 
     /*VERSÃO MPI*/
     
